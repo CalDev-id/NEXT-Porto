4758ba8f25b3e8e4ca62427408fee7ae1ee924d9
Heical Chandra <haical31@gmail.com>
2025-11-25 15:54:35 +0700

    update

---
+++ src/app/sitemap.xml/route.js
@@ -0,0 +1,18 @@
+export async function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://caldev.my.id/</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <changefreq>weekly</changefreq>
+      <priority>1.0</priority>
+    </url>
+  </urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, max-age=0, must-revalidate",
+    },
+  });
+}
